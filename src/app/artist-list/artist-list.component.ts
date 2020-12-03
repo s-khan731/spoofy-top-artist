62f12ca3bba99cce04eb9eeedb9300c1c5393489
s-khan731 <khanicakes@gmail.com>
2020-12-02 20:20:50 -0500

    create playlists

--- src/app/artist-list/artist-list.component.ts
+++ src/app/artist-list/artist-list.component.ts
@@ -12,8 +12,9 @@ export class ArtistListComponent implements OnInit {
   code = '';
   artists = [];
   songs = [];
+  userId = '';
   logInUrl = "https://accounts.spotify.com/authorize?client_id=af93ffba55fe40c39a8dc77f025c17c9&response_type=code&redirect_uri=https://spoofy-top-artist.stackblitz.io" + 
-    "&user-top-read user-read-private user-read-email playlist-modify-public playlist-modify-private";
+    "&scope=user-top-read user-read-private user-read-email playlist-modify-public playlist-modify-private";
   showDataForAllTime = false;
   constructor(public artistService: ArtistListService, private activatedRoute: ActivatedRoute) {
   this.activatedRoute.queryParams.subscribe(params => {
@@ -49,16 +50,25 @@ export class ArtistListComponent implements OnInit {
         console.error(error);
         console.log('error retrieving artist data')
       }
-    )
+    );
     this.artistService.retrieveSongs().subscribe(
       result => {
         this.songs = result.items;
       },
       error => {
         console.error(error);
-        console.log('error retrieving artist data')
+        console.log('error retrieving track data')
       }
-    )
+    );
+    this.artistService.getUserData().subscribe(
+      result => {
+        this.userId = result.id;
+      },
+      error => {
+        console.error(error);
+        console.log('error retrieving user data')
+      }
+    );
   }
 
   toggleView() {
@@ -71,7 +81,29 @@ export class ArtistListComponent implements OnInit {
   }
 
   generatePlaylist() {
-
+    this.artistService.createPlaylist(this.userId).subscribe(
+      result => {
+        let playlistId = result.id;
+        console.log('playlist created successfully!');
+        let songIds = [];
+        for (let song of this.songs) {
+          songIds.push(song.uri);
+        }
+        this.artistService.generatePlaylist(playlistId, songIds).subscribe(
+          result => {
+            console.log('added songs successfully!');
+          },
+          error => {
+                console.error(error);
+                console.log('error adding songs to playlist')
+              }
+            );
+      },
+      error => {
+        console.error(error);
+        console.log('error creating playlist')
+      }
+    );
   }
 
 }
--- src/app/artist-list/artist-list.service.ts
+++ src/app/artist-list/artist-list.service.ts
@@ -42,8 +42,28 @@ export class ArtistListService {
     return this.http.get(path, {headers: header});
   }
 
-  public createPlaylist(): any {
-    const path = "https://api.spotify.com/v1/me/top/tracks?limit=50&time_range=" + this.timeRange + "&offset=" + this.offset;
+  public createPlaylist(userId: string): any {
+    const path = "https://api.spotify.com/v1/users/" + userId + "/playlists";
+    const body = {
+      "name": "Spoofy Top Tracks!",
+      "description": "made by surrah :)",
+      "public": "false"
+    };
+    let header = new HttpHeaders();
+    header = header.append('Authorization', this.tokenType + ' ' + this.accessToken);
+    return this.http.post(path, body, {headers: header});
+  }
+
+  public generatePlaylist(playlistId: string, songs: string[]): any {
+    const path = "https://api.spotify.com/v1/playlists/" + playlistId + "/tracks";
+    const body = JSON.stringify(songs);
+    let header = new HttpHeaders();
+    header = header.append('Authorization', this.tokenType + ' ' + this.accessToken);
+    return this.http.post(path, body, {headers: header});
+  }
+
+  public getUserData(): any {
+     const path = "https://api.spotify.com/v1/me/";
     let header = new HttpHeaders();
     header = header.append('Authorization', this.tokenType + ' ' + this.accessToken);
     return this.http.get(path, {headers: header});
